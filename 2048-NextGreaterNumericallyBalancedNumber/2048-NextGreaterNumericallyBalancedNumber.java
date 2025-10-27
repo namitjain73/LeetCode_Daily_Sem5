c810b82207e221486bf098e716dae4158d48b924
Namit jain <namitjain9838@gmail.com>
2025-10-28 00:18:59 +0530

    Create 2048-NextGreaterNumericallyBalancedNumber

---
+++ 2048-NextGreaterNumericallyBalancedNumber/2048-NextGreaterNumericallyBalancedNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 10/28/2025, 12:18:47 AM
+class Solution {
+    public boolean solve(int x){
+        String s = String.valueOf(x);
+        int[] vec = new int[10];
+        for (char ch : s.toCharArray()) vec[ch - '0']++;
+        for (char ch : s.toCharArray()){
+            int c = ch - '0';
+            if (c == 0 || vec[c] != c) return false;
+        }
+        return true;
+    }
+    public int nextBeautifulNumber(int n) {
+        for (int i = n + 1; ; i++) if (solve(i)) return i;
+    }
+}
