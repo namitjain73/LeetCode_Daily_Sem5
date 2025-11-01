e581d720a38e3c3c8874c6c65b899bb3790957a9
Namit jain <namitjain9838@gmail.com>
2025-11-01 22:52:53 +0530

    Create 3217-DeleteNodesFromLinkedListPresentinArray

---
+++ 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.java
@@ -0,0 +1,25 @@
+// Last updated: 11/1/2025, 10:52:52 PM
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        int max = -1;
+        for (int num : nums) max = Math.max(max, num);
+        boolean[] freq = new boolean[max + 1];  
+        
+        for (int num : nums) freq[num] = true;
+
+        ListNode temp = new ListNode();
+        ListNode curr = temp;
+
+        while (head != null) {
+            if (head.val >= freq.length || freq[head.val] == false) {
+                curr.next = head;
+                curr = curr.next;
+            }
+
+            head = head.next;
+        }
+
+        curr.next = null;
+        return temp.next;
+    }
+}
