fdb0c430cba341083490d8b929d7f6eacb3edc61
Namit jain <namitjain9838@gmail.com>
2025-12-05 23:26:08 +0530

    File updated at 12/5/2025, 11:26:07 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.java
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.java
@@ -1,14 +1,19 @@
-// Last updated: 12/5/2025, 11:23:44 PM
+// Last updated: 12/5/2025, 11:26:07 PM
 1class Solution {
-2    public int[] shuffle(int[] nums, int n) {
-3        int[] ans = new int[2*n];
-4        int j = 0;
-5        for(int i = 0 ; i < 2*n ; i+=2){
-6            //System.out.println(i);
-7            ans[i] = nums[j];
-8            ans[i+1] = nums[j+n];
-9            j++;
-10        }
-11        return ans;
-12    }
-13}
+2    public int findMaxConsecutiveOnes(int[] nums) {
+3        int res = 0;
+4        int n = nums.length;
+5        int ans = 0;
+6
+7        for(int i = 0 ; i < n ;i++){
+8            if(nums[i] == 1){
+9                ans++;
+10            }
+11            else if(nums[i] == 0){
+12                res = Math.max(res,ans);
+13                ans = 0;
+14            }
+15        }
+16        return Math.max(ans,res);
+17    }
+18}
