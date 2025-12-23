828553486fc2f46c0c96c906d7e3cf6240c6246d
Namit jain <namitjain9838@gmail.com>
2025-12-23 14:17:12 +0530

    Create 3582-GenerateTagforVideoCaption

---
+++ 3582-GenerateTagforVideoCaption/3582-GenerateTagforVideoCaption.java
@@ -0,0 +1,23 @@
+// Last updated: 12/23/2025, 2:17:11 PM
+1class Solution {
+2    public String generateTag(String s) {
+3        s = s.trim();
+4        if(s.length() == 0) return "#";
+5        StringBuilder sb = new StringBuilder("#");
+6        sb.append(Character.toString(s.charAt(0)).toLowerCase());
+7        for(int i = 1 ; i < s.length(); i++){
+8            if(s.charAt(i) == ' ') {
+9                continue;
+10            }
+11            if(s.charAt(i-1) == ' '){
+12                sb.append(Character.toString(s.charAt(i)).toUpperCase());
+13            }else {
+14                sb.append(Character.toString(s.charAt(i)).toLowerCase());
+15            }
+16            if(sb.length() == 100) break;
+17        }
+18
+19        return sb.toString();
+20    }
+21}
+22
