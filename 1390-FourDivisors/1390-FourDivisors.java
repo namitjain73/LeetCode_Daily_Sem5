b17e6c01ee1fa2ca667be729b645dbc63c78a933
Namit jain <namitjain9838@gmail.com>
2026-01-05 00:30:55 +0530

    File updated at 1/5/2026, 12:30:55 AM

--- 1390-FourDivisors/1390-FourDivisors.java
+++ 1390-FourDivisors/1390-FourDivisors.java
@@ -1,24 +1,24 @@
-// Last updated: 10/30/2025, 2:18:27 AM
-class Solution {
-    public int sumFourDivisors(int[] nums) {
-        int sum = 0;
-        for (int num : nums) {
-            List<Integer> res = new ArrayList<>();
-            for (int j = 1; j * j <= num; j++) {
-                if (num % j == 0) {
-                    res.add(j);
-                    if (j != num / j) { 
-                        res.add(num / j);
-                    }
-                }
-                if (res.size() > 4) break;
-            }
-            if (res.size() == 4) {
-                for (int k : res) {
-                    sum += k;
-                }
-            }
-        }
-        return sum;
-    }
-}
+// Last updated: 1/5/2026, 12:30:55 AM
+1class Solution {
+2    public int sumFourDivisors(int[] nums) {
+3        int sum = 0;
+4        for (int num : nums) {
+5            List<Integer> res = new ArrayList<>();
+6            for (int j = 1; j * j <= num; j++) {
+7                if (num % j == 0) {
+8                    res.add(j);
+9                    if (j != num / j) { 
+10                        res.add(num / j);
+11                    }
+12                }
+13                if (res.size() > 4) break;
+14            }
+15            if (res.size() == 4) {
+16                for (int k : res) {
+17                    sum += k;
+18                }
+19            }
+20        }
+21        return sum;
+22    }
+23}
