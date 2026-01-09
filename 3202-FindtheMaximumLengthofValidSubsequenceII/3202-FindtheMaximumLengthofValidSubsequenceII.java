b2878a1abd7dac62104c169fde681f76ef9d969a
Namit jain <namitjain9838@gmail.com>
2026-01-09 23:42:46 +0530

    File updated at 1/9/2026, 11:42:45 PM

--- 3202-FindtheMaximumLengthofValidSubsequenceII/3202-FindtheMaximumLengthofValidSubsequenceII.java
+++ 3202-FindtheMaximumLengthofValidSubsequenceII/3202-FindtheMaximumLengthofValidSubsequenceII.java
@@ -1,17 +1,43 @@
-// Last updated: 1/9/2026, 11:42:17 PM
+// Last updated: 1/9/2026, 11:42:45 PM
 1class Solution {
-2    public int maximumLength(int[] nums, int k) {
-3        int[][] dp = new int[nums.length][k];
-4        for(int[] dpin : dp) Arrays.fill(dpin,1);
-5
-6        int max=0;
-7        for(int i=0 ; i<nums.length ; i++){
-8            for(int j=i-1 ; j>=0 ; j--){
-9                int rem = (nums[i]+nums[j])%k;
-10                dp[i][rem] = Math.max(dp[i][rem],dp[j][rem]+1);
-11                max = Math.max(max,dp[i][rem]);
-12            }
-13        }
-14        return max;
-15    }
-16}
+2    public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
+3        int[][] dp = new int[words.length][2];
+4        for(int i=0 ; i<words.length ; i++){
+5            dp[i][0] = 1;
+6            dp[i][1] = i;
+7        }
+8        for(int i=0 ; i<words.length; i++){
+9            for(int j=i-1 ; j>=0 ; j--){
+10                if(groups[i]!=groups[j] && dist(words[i],words[j]) && dp[j][0]+1>dp[i][0]){
+11                    dp[i][0] = dp[j][0]+1;
+12                    dp[i][1] = j;
+13                }
+14            }
+15        }
+16        int idx=0;
+17        for(int i=0 ; i<words.length ; i++){
+18            if(dp[i][0]>dp[idx][0]){
+19                idx = i;
+20            }
+21        }
+22
+23        List<String> ans = new ArrayList<>();
+24        int temp=idx;
+25        for(int i=0 ; i<dp[idx][0] ; i++){
+26            ans.add(0,words[temp]);
+27            temp = dp[temp][1];
+28        }
+29        return ans;
+30    }
+31    public boolean dist(String s1,String s2){
+32        if(s1.length()!=s2.length()) return false;
+33        boolean flag = true;
+34        for(int i=0 ; i<s1.length() ; i++){
+35            if(s1.charAt(i)!=s2.charAt(i)){
+36                if(flag) flag = false;
+37                else return false;
+38            }
+39        }
+40        return true;
+41    }
+42}
