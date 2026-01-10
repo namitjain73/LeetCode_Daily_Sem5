6911256a325f903135199c8eca953a2bd287dbed
Namit jain <namitjain9838@gmail.com>
2026-01-10 23:22:59 +0530

    File updated at 1/10/2026, 11:22:58 PM

--- 1488-AvoidFloodinTheCity/1488-AvoidFloodinTheCity.java
+++ 1488-AvoidFloodinTheCity/1488-AvoidFloodinTheCity.java
@@ -1,27 +1,22 @@
-// Last updated: 1/10/2026, 11:17:34 PM
+// Last updated: 1/10/2026, 11:22:58 PM
 1class Solution {
-2
-3    public int[] avoidFlood(int[] rains) {
-4        int[] ans = new int[rains.length];
-5        Arrays.fill(ans, 1);
-6        TreeSet<Integer> st = new TreeSet<Integer>();
-7        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
-8        for (int i = 0; i < rains.length; ++i) {
-9            if (rains[i] == 0) {
-10                st.add(i);
-11            } else {
-12                ans[i] = -1;
-13                if (mp.containsKey(rains[i])) {
-14                    Integer it = st.ceiling(mp.get(rains[i]));
-15                    if (it == null) {
-16                        return new int[0];
-17                    }
-18                    ans[it] = rains[i];
-19                    st.remove(it);
-20                }
-21                mp.put(rains[i], i);
-22            }
-23        }
-24        return ans;
-25    }
-26}
+2    public int longestArithSeqLength(int[] nums) {
+3        int n = nums.length;
+4        if (n <= 2)
+5            return n;
+6
+7        int longest = 2;
+8        Map<Integer, Integer>[] dp = new HashMap[n];
+9
+10        for (int i = 0; i < n; i++) {
+11            dp[i] = new HashMap<>();
+12            for (int j = 0; j < i; j++) {
+13                int diff = nums[i] - nums[j];
+14                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
+15                longest = Math.max(longest, dp[i].get(diff));
+16            }
+17        }
+18
+19        return longest;
+20    }
+21}
