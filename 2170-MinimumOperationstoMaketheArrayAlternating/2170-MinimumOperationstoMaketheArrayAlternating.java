4b3570f48d82da02a28f424b25e5095d213aad02
Namit jain <namitjain9838@gmail.com>
2026-01-18 23:34:44 +0530

    File updated at 1/18/2026, 11:34:43 PM

--- 2170-MinimumOperationstoMaketheArrayAlternating/2170-MinimumOperationstoMaketheArrayAlternating.java
+++ 2170-MinimumOperationstoMaketheArrayAlternating/2170-MinimumOperationstoMaketheArrayAlternating.java
@@ -1,4 +1,4 @@
-// Last updated: 1/12/2026, 11:32:48 PM
+// Last updated: 1/18/2026, 11:34:43 PM
 1class Solution {
 2    public int minimumOperations(int[] nums) {
 3        HashMap<Integer,Integer> freq1 =  new HashMap<>();
@@ -15,60 +15,45 @@
 14        for(int i = 0 ; i < n ; i++){
 15            if(i % 2 == 0){
 16                freq1.put(nums[i] , freq1.getOrDefault(nums[i],0)+1);
-17                // if(a <= freq1.get(nums[i]) ){
-18                //     pre_a = a;
-19                //     a = freq1.get(nums[i]);
-20                //     max_a = nums[i];
-21                // }
-22            }
-23            else {
-24                freq2.put(nums[i] , freq2.getOrDefault(nums[i],0)+1);
-25                // if(b <= freq2.get(nums[i])){
-26                //     pre_b = b;
-27                //     b = freq2.get(nums[i]);
-28                //     max_b = nums[i];
-29                // }
-30            }
-31            // System.out.println(max_a +" " + pre_a + " " + max_b+" " + pre_b);
-32            // if(max_a != max_b){
-33            //     ans = Math.min(ans , n - (a+b));
-34            // }else{
-35            //     ans = Math.min(ans , n - (pre_a+pre_b));
-36            // }
-37        }
-38
-39        for(int i : freq1.keySet()){
-40            if(max_a <= freq1.get(i)){
-41                pre_a = max_a;
-42                max_a = freq1.get(i);
-43                a = i;
-44            }else if(freq1.get(i) < max_a && freq1.get(i) > pre_a){
-45                pre_a = freq1.get(i);
-46            }
-47        }
-48
-49        for(int i : freq2.keySet()){
-50            if(max_b <= freq2.get(i)){
-51                pre_b = max_b;
-52                max_b = freq2.get(i);
-53                b = i;
-54            }else if(freq2.get(i) < max_b && freq2.get(i) > pre_b){
-55                pre_b = freq2.get(i);
-56            }
-57
-58            if(a == b){
-59                ans = Math.min(ans,n - (Math.max(max_a+pre_b , max_b+pre_a)));
-60            }else{
-61                ans = Math.min(ans, n - (max_a+max_b));
-62            }
-63        }
-64        if(a == b){
-65            ans = Math.min(ans,n - (Math.max(max_a+pre_b , max_b+pre_a)));
-66        }else{
-67            ans = Math.min(ans, n - (max_a+max_b));
-68        }
-69
-70
-71        return ans;
-72    }
-73}
+17            }
+18            else {
+19                freq2.put(nums[i] , freq2.getOrDefault(nums[i],0)+1);
+20            }
+21            
+22        }
+23
+24        for(int i : freq1.keySet()){
+25            if(max_a <= freq1.get(i)){
+26                pre_a = max_a;
+27                max_a = freq1.get(i);
+28                a = i;
+29            }else if(freq1.get(i) < max_a && freq1.get(i) > pre_a){
+30                pre_a = freq1.get(i);
+31            }
+32        }
+33
+34        for(int i : freq2.keySet()){
+35            if(max_b <= freq2.get(i)){
+36                pre_b = max_b;
+37                max_b = freq2.get(i);
+38                b = i;
+39            }else if(freq2.get(i) < max_b && freq2.get(i) > pre_b){
+40                pre_b = freq2.get(i);
+41            }
+42
+43            if(a == b){
+44                ans = Math.min(ans,n - (Math.max(max_a+pre_b , max_b+pre_a)));
+45            }else{
+46                ans = Math.min(ans, n - (max_a+max_b));
+47            }
+48        }
+49        if(a == b){
+50            ans = Math.min(ans,n - (Math.max(max_a+pre_b , max_b+pre_a)));
+51        }else{
+52            ans = Math.min(ans, n - (max_a+max_b));
+53        }
+54
+55
+56        return ans;
+57    }
+58}
