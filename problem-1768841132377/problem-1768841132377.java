84f69d1fcd2f4a6fb7072ab0408cb75f255d2941
Namit jain <namitjain9838@gmail.com>
2026-01-19 22:15:33 +0530

    Create

---
+++ problem-1768841132377/problem-1768841132377.java
@@ -0,0 +1,22 @@
+// Last updated: 1/19/2026, 10:15:32 PM
+1class Solution {
+2    public int longestBalanced(int[] nums) {
+3        int ans = 0;
+4        int n = nums.length;
+5        for(int i = 0 ; i < n ; i++){
+6        Set<Integer> odd = new HashSet<>();
+7        Set<Integer> even = new HashSet<>();
+8            for(int j = i ; j < n ; j++){
+9                if(nums[j] % 2 == 0){
+10                    even.add(nums[j]);
+11                }else{
+12                    odd.add(nums[j]);
+13                }
+14                if(even.size() == odd.size()){
+15                    ans = Math.max(ans , j - i + 1);
+16                }
+17            }
+18        }
+19        return ans;
+20    }
+21}
