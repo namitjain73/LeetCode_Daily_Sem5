c9b2cee7c570b419aa74926388193321709dc0f4
Namit jain <namitjain9838@gmail.com>
2026-01-21 12:40:01 +0530

    Create 3314-ConstructtheMinimumBitwiseArrayI

---
+++ 3314-ConstructtheMinimumBitwiseArrayI/3314-ConstructtheMinimumBitwiseArrayI.java
@@ -0,0 +1,16 @@
+// Last updated: 1/21/2026, 12:40:00 PM
+1class Solution {
+2    public int[] minBitwiseArray(List<Integer> nums) {
+3        int n = nums.size();
+4        int[] ans = new int[n];
+5        for(int i = 0 ; i < n;  i++){
+6            int num = nums.get(i);
+7            if((num & 1) == 1){
+8                ans[i] = num & ~(((num+1) & ~num) >> 1);
+9            }else{
+10                ans[i] =-1;
+11            }
+12        }
+13        return ans;
+14    }
+15}
