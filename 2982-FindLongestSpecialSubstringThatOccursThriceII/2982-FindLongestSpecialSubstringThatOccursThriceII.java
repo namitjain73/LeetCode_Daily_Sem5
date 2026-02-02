d7f528f1283c211b9ca594cdfad01e3d11117137
Namit jain <namitjain9838@gmail.com>
2026-02-02 23:24:32 +0530

    File updated at 2/2/2026, 11:24:31 PM

--- 2982-FindLongestSpecialSubstringThatOccursThriceII/2982-FindLongestSpecialSubstringThatOccursThriceII.java
+++ 2982-FindLongestSpecialSubstringThatOccursThriceII/2982-FindLongestSpecialSubstringThatOccursThriceII.java
@@ -1,31 +1,31 @@
-// Last updated: 1/20/2026, 10:15:35 PM
+// Last updated: 2/2/2026, 11:24:31 PM
 1class Solution {
 2    public int maximumLength(String s) {
 3        int n = s.length();
-4        int[][] freq = new int[26][n + 1];
-5        char pre = s.charAt(0);
+4        int[][] freq = new int[26][n+1];
+5        int pre = s.charAt(0);
 6        freq[s.charAt(0)-'a'][1] = 1;
 7        int len = 1;
 8
 9        for(int i = 1 ; i < n ; i++){
-10            char curr = s.charAt(i);
-11            if(curr == pre){
+10            int curr = s.charAt(i);
+11            if(pre == curr){
 12                len++;
 13                freq[curr-'a'][len]+=1;
 14            }else{
-15                freq[curr-'a'][1] += 1;
-16                pre = curr;
-17                len = 1;
+15                len = 1;
+16                freq[curr-'a'][1] += 1;
+17                pre = curr;
 18            }
 19        }
 20
 21        int ans = -1;
 22        for(int i = 0 ; i < 26 ; i++){
 23            int sum = 0;
-24            for(int j = n; j >= 0 ; j--){
+24            for(int j = n ; j >= 0 ; j--){
 25                sum += freq[i][j];
 26                if(sum >= 3){
-27                    ans = Math.max(ans , j);
+27                    ans = Math.max(ans,j);
 28                    break;
 29                }
 30            }
