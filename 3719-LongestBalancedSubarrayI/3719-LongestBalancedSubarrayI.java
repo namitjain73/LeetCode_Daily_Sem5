b85604e17b61804dc7f66acdcbe995643eab5b49
Namit jain <namitjain9838@gmail.com>
2026-02-11 02:58:16 +0530

    File updated at 2/11/2026, 2:58:15 AM

--- 3719-LongestBalancedSubarrayI/3719-LongestBalancedSubarrayI.java
+++ 3719-LongestBalancedSubarrayI/3719-LongestBalancedSubarrayI.java
@@ -1,23 +1,22 @@
-// Last updated: 1/12/2026, 11:53:10 PM
+// Last updated: 2/11/2026, 2:58:15 AM
 1class Solution {
 2    public int longestBalanced(int[] nums) {
-3        int ans=0;
-4        int n=nums.length;
-5        Set<Integer> odd,even;
-6        for(int i=0;i<n;i++){
-7            odd=new HashSet<>();
-8            even=new HashSet<>();
-9            for(int j=i;j<n;j++){
-10                if(nums[j]%2==0){
-11                    even.add(nums[j]);
-12                }else{
-13                    odd.add(nums[j]);
-14                }
-15                if(even.size()==odd.size()){
-16                    ans=Math.max(ans,j-i+1);
-17                }
-18            }
-19        }
-20        return ans;
-21    }
-22}
+3        int ans = 0;
+4        int n = nums.length;
+5        for(int i = 0 ; i < n ; i++){
+6        Set<Integer> odd = new HashSet<>();
+7        Set<Integer> even = new HashSet<>();
+8            for(int j = i ; j < n ; j++){
+9                if(nums[j] % 2 == 0){
+10                    even.add(nums[j]);
+11                }else{
+12                    odd.add(nums[j]);
+13                }
+14                if(even.size() == odd.size()){
+15                    ans = Math.max(ans , j - i + 1);
+16                }
+17            }
+18        }
+19        return ans;
+20    }
+21}
