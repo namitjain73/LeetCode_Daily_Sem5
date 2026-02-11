828f8f40493624f4ca188732b43ce11f18d056aa
Namit jain <namitjain9838@gmail.com>
2026-02-12 01:55:12 +0530

    File updated at 2/12/2026, 1:55:10 AM

--- 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.java
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.java
@@ -1,37 +1,36 @@
-// Last updated: 8/22/2025, 3:16:07 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int max_depth;
-    public List<Integer> rightSideView(TreeNode root) {
-        max_depth = 0;
-        List<Integer> ll = new ArrayList<>();
-        helper(ll,1,root);
-        return ll;
-    }
-
-    public void helper(List<Integer> list , int curr_node , TreeNode root){
-        if(root == null) return;
-
-        if(max_depth < curr_node){
-            max_depth = curr_node;
-            list.add(root.val);
-        }
-
-        helper(list , curr_node+1 , root.right);
-        helper(list , curr_node + 1 , root.left);
-    }
-}
+// Last updated: 2/12/2026, 1:55:10 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    List<Integer> ll;
+18    List<Integer> check;
+19    public void solver(TreeNode root, int n){
+20        if(root == null) return;
+21
+22        if(!check.contains(n)){
+23            check.add(n);
+24            ll.add(root.val);
+25        }
+26        solver(root.right,n+1);
+27        solver(root.left,n+1);
+28    }
+29    public List<Integer> rightSideView(TreeNode root) {
+30        ll =new ArrayList<>();
+31        check = new ArrayList<>();
+32        solver(root,0);
+33        return ll;
+34    }
+35}
