12e1a018f96b75fdfe551e9bc94c1efd7eff7eca
Namit jain <namitjain9838@gmail.com>
2026-02-20 02:08:06 +0530

    Create 696-CountBinarySubstrings

---
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.java
@@ -0,0 +1,26 @@
+// Last updated: 2/20/2026, 2:08:06 AM
+1class Solution {
+2    public int countBinarySubstrings(String s) {
+3        int n = s.length();
+4        int[] blocks = new int[n];
+5        int idx = 0;
+6        
+7        int count = 1;
+8        for (int i = 1; i < n; i++) {
+9            if (s.charAt(i) == s.charAt(i - 1)) {
+10                count++;
+11            } else {
+12                blocks[idx++] = count;
+13                count = 1;
+14            }
+15        }
+16        blocks[idx++] = count;
+17        
+18        int res = 0;
+19        for (int i = 0; i < idx - 1; i++) {
+20            res += Math.min(blocks[i], blocks[i + 1]);
+21        }
+22        
+23        return res;
+24    }
+25}
