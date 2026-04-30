acb92bc70db055a46728d9e166e8efc13adf09c5
Namit jain <namitjain9838@gmail.com>
2026-04-30 17:56:53 +0530

    File updated at 4/30/2026, 5:56:50 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,27 +1,19 @@
-// Last updated: 4/30/2026, 5:55:59 PM
+// Last updated: 4/30/2026, 5:56:50 PM
 1class Solution {
 2    public int lengthOfLongestSubstring(String s) {
 3        int l = 0;
-4        int r = 0;
-5
-6        int[] arr =new int[256];
-7
-8        int max = 0;
-9
-10        while(r < s.length()) {
-11            int ch = s.charAt(r);
-12            arr[ch]++;
-13            while(arr[ch] > 1) {
-14                arr[s.charAt(l)]--;
-15                l++;
-16            }
-17            
-18
-19            max = Math.max(max, r - l+ 1);
-20            r++;
-21        }
-22
-23        return max;
-24        
-25    }
-26}
+4        int sum = 0;
+5        int max = 0;
+6        int n = s.length();
+7        int[] arr = new int[256];
+8        for(int r = 0; r < n; r++){
+9            arr[s.charAt(r)]++;
+10            while(arr[s.charAt(r)] > 1){
+11                arr[s.charAt(l)]--;
+12                l++;
+13            }
+14            max = Math.max(max , r-l+1);
+15        }
+16        return max;
+17    }
+18}
