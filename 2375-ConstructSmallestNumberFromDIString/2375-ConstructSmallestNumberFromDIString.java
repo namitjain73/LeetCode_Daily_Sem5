740d535180a516039e15a7c47fb68f9b305cd781
Namit jain <namitjain9838@gmail.com>
2026-05-09 20:41:16 +0530

    File updated at 5/9/2026, 8:41:15 PM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
@@ -1,47 +1,27 @@
-// Last updated: 5/9/2026, 8:24:17 PM
-1class Solution {
-2    public int maximalRectangle(char[][] matrix) {
-3        int ans = 0;
-4        int n = matrix.length;
-5        int[] arr = new int[matrix[0].length];
-6        for(int i = n-1 ; i >= 0 ; i--){
-7            for(int j = 0 ; j < matrix[0].length ; j++){
-8                if(matrix[i][j] == '0') arr[j]= 0;
-9                else arr[j]++;
-10            }
-11            ans = Math.max(ans , rectangle(arr));
-12        }
-13        return ans;
-14    }
-15    public int rectangle(int[] arr){
-16        Stack<Integer> st = new Stack<>();
-17        int ans= 0;
-18
-19
-20        for(int i = 0 ; i < arr.length ; i++){
-21            while(!st.isEmpty() && arr[i] < arr[st.peek()]){
-22                int h = arr[st.pop()];
-23                int r = i;
-24                if(st.isEmpty()){
-25                    ans = Math.max(ans , r*h);
-26                }else{
-27                    int l = st.peek();
-28                    ans = Math.max(ans , h * (r - l - 1));
-29                }
-30            }
-31            st.push(i);
-32        }
-33
-34        int r = arr.length;
-35        while(!st.isEmpty()){
-36            int h = arr[st.pop()];
-37            if(st.isEmpty()){
-38                ans = Math.max(ans , r*h);
-39            }else{
-40                int l= st.peek();
-41                ans = Math.max(ans , h * (r - l - 1));
-42            }
-43        }
-44        return ans;
-45    }
-46}
+// Last updated: 5/9/2026, 8:41:15 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean hasPathSum(TreeNode root, int targetSum) {
+18        if(root == null) return false;
+19        if(root.left == null && root.right == null && root.val == targetSum) return true;
+20
+21
+22        boolean left = hasPathSum(root.left , targetSum - root.val);
+23        boolean right = hasPathSum(root.right,targetSum - root.val);
+24        return left || right;
+25    }
+26}
