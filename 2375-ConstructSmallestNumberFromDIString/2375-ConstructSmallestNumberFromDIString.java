e081205b24adb6702ce20602bed57d10daf2c7df
Namit jain <namitjain9838@gmail.com>
2026-05-10 01:30:59 +0530

    File updated at 5/10/2026, 1:30:58 AM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
@@ -1,4 +1,4 @@
-// Last updated: 5/10/2026, 1:14:00 AM
+// Last updated: 5/10/2026, 1:30:58 AM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,17 +15,26 @@
 14 * }
 15 */
 16class Solution {
-17    int ans = 0;
-18    public int distributeCoins(TreeNode root) {
-19        ans = 0;
-20        solver(root);
-21        return ans;
+17    int camera = 0;
+18    public int minCameraCover(TreeNode root) {
+19        int c = minCamera(root);
+20        if(c == -1 ) camera++;
+21        return camera;
 22    }
-23    public int solver(TreeNode root){
-24        if(root == null) return 0;
-25        int left = solver(root.left);
-26        int right = solver(root.right);
-27        ans += Math.abs(right)+Math.abs(left);
-28        return root.val+left+right-1;
-29    }
-30}
+23
+24    public int minCamera(TreeNode root){
+25        if(root == null) return 0;
+26
+27
+28        int left = minCamera(root.left);
+29        int right = minCamera(root.right);
+30
+31        if(left == -1 || right == -1) {
+32            camera++;
+33            return 1;
+34        }
+35        else if(left == 1 || right == 1) return 0;
+36        else return -1;
+37        // return root;
+38    }
+39}
