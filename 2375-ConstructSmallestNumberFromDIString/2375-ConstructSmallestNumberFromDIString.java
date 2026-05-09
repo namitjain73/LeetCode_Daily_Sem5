e714cf63df36790e129973c5cb626374168f92ce
Namit jain <namitjain9838@gmail.com>
2026-05-09 15:29:32 +0530

    File updated at 5/9/2026, 3:29:32 PM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
@@ -1,23 +1,20 @@
-// Last updated: 5/9/2026, 12:55:38 PM
-1class Solution {
-2    public String smallestNumber(String pattern) {
-3        int n = pattern.length();
-4        int[] arr = new int[n+1];
-5        Stack<Integer> st = new Stack<>();
-6        int c = 1;
-7
-8        for(int i = 0 ; i <= n ; i++){
-9            if(i == n || pattern.charAt(i) == 'I'){
-10                arr[i] = c;
-11                c++;
-12                while(!st.isEmpty()){
-13                    arr[st.pop()] =  c;
-14                    c++;
-15                }
-16            }else if(pattern.charAt(i) == 'D') st.push(i);
-17        }
-18        StringBuilder sb = new StringBuilder();
-19        for(int i = 0 ; i <= n ; i++) sb.append(arr[i]);
-20        return sb.toString();
-21    }
-22}
+// Last updated: 5/9/2026, 3:29:32 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) { val = x; }
+7 * }
+8 */
+9class Solution {
+10    public void deleteNode(ListNode node) {
+11        ListNode temp = node;
+12        while(temp.next.next != null){
+13            temp.val = temp.next.val;
+14            temp = temp.next;
+15        }
+16        temp.val = temp.next.val;
+17        temp.next = null;
+18    }
+19}
