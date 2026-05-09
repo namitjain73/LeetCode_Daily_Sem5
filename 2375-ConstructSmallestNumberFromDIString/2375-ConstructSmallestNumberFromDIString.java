572fb1317d59f7f275a86efe84ebe0331366de99
Namit jain <namitjain9838@gmail.com>
2026-05-09 15:36:11 +0530

    File updated at 5/9/2026, 3:36:10 PM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
@@ -1,20 +1,27 @@
-// Last updated: 5/9/2026, 3:29:32 PM
+// Last updated: 5/9/2026, 3:36:10 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode(int x) { val = x; }
-7 * }
-8 */
-9class Solution {
-10    public void deleteNode(ListNode node) {
-11        ListNode temp = node;
-12        while(temp.next.next != null){
-13            temp.val = temp.next.val;
-14            temp = temp.next;
-15        }
-16        temp.val = temp.next.val;
-17        temp.next = null;
-18    }
-19}
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+14        ListNode a = headA;
+15        ListNode b = headB;
+16
+17        while(a != b){
+18            if(a == null) a = headA;
+19            else a = a.next;
+20            if(b == null) b = headB;
+21            else b = b.next;
+22        }
+23        return a;
+24
+25    }
+26}
