48ce2aa099433621ae61e1eb8d77be93795c97f7
Namit jain <namitjain9838@gmail.com>
2026-05-09 18:41:53 +0530

    File updated at 5/9/2026, 6:41:52 PM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.java
@@ -1,27 +1,55 @@
-// Last updated: 5/9/2026, 3:36:10 PM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode(int x) {
-7 *         val = x;
-8 *         next = null;
-9 *     }
-10 * }
-11 */
-12public class Solution {
-13    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-14        ListNode a = headA;
-15        ListNode b = headB;
-16
-17        while(a != b){
-18            if(a == null) a = headA;
-19            else a = a.next;
-20            if(b == null) b = headB;
-21            else b = b.next;
-22        }
-23        return a;
-24
-25    }
-26}
+// Last updated: 5/9/2026, 6:41:52 PM
+1class Solution {
+2    public ListNode reverseKGroup(ListNode head, int k) {
+3        ListNode temp = head;
+4        ListNode next = null;
+5        ListNode kth = null;
+6        ListNode pre = null;
+7
+8
+9        while(temp != null){
+10            kth = kthNode(temp,k);
+11            if(kth == null){
+12                if(pre  != null){
+13                    pre.next = temp;
+14                    break;
+15                }
+16            }
+17
+18            next = kth.next;
+19            kth.next = null;
+20            reverse(temp);
+21            if(temp == head){
+22                head = kth;
+23
+24            }else{
+25                pre.next = kth;
+26            }
+27
+28            pre = temp;
+29            temp = next;
+30        }
+31        return head;
+32    }
+33
+34    public ListNode kthNode(ListNode head , int n){
+35        n--;
+36        while(head != null && n-- > 0){
+37            head = head.next;
+38        }
+39
+40        return head;
+41    }
+42
+43    public void reverse(ListNode head){
+44        ListNode pre = null;
+45        ListNode temp = head;
+46
+47        while(temp != null){
+48            ListNode curr = temp.next;
+49            temp.next = pre;
+50            pre = temp;
+51            temp = curr;
+52        }
+53    }
+54}
