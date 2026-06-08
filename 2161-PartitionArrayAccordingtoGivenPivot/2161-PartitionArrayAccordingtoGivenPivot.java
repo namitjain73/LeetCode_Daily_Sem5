fb218d21bb93d1135f06f53cba972f2868544f61
Namit jain <namitjain9838@gmail.com>
2026-06-08 12:40:40 +0530

    File updated at 6/8/2026, 12:40:17 PM

--- 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
@@ -1,23 +1,23 @@
-// Last updated: 6/8/2026, 12:23:04 PM
+// Last updated: 6/8/2026, 12:40:17 PM
 1class Solution {
 2    public int[] pivotArray(int[] nums, int pivot) {
-3        int k = 0;
-4        int n = nums.length;
-5        int[] arr= new int[n];
-6        for(int i = 0 ; i < n;  i++){
-7            if(nums[i] < pivot){
-8                arr[k++] = nums[i];
-9            }
-10        }
-11        for(int i = 0 ; i < n ; i++){
-12            if(nums[i] == pivot){
-13                arr[k++] = nums[i];
-14            }
-15        }
-16        for(int i = 0 ; i < n ; i++){
-17            if(nums[i] > pivot){
-18                arr[k++] = nums[i];
-19            }
+3        int n = nums.length;
+4        int[] arr = new int[n];
+5        int k = 0;
+6        Queue<Integer> q= new LinkedList<>();
+7        int equal = 0;
+8        for(int i = 0 ; i < n ; i++){
+9            if(nums[i] == pivot) equal++;
+10            else if(nums[i] > pivot) q.add(nums[i]);
+11            else {
+12                arr[k++] = nums[i];
+13            }
+14        }
+15        while(equal-- > 0){
+16            arr[k++] = pivot;
+17        }
+18        while(!q.isEmpty()){
+19            arr[k++] = q.poll();
 20        }
 21        return arr;
 22    }
