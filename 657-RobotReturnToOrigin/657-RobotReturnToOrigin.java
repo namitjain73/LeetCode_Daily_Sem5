d468ec57505166dc339b438d33cc6376c1d0d3c2
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:20:40 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 5:20:35 PM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0;
+        int y=0;
+        for (int i = 0;i<moves.length();i++){
+            char ch = moves.charAt(i);
+            if (ch=='U'){
+                y+=1;
+            }
+            else if (ch=='D'){
+                    y-=1;
+            }
+            else if (ch=='L'){
+                    x-=1;
+            }
+            else if (ch=='R'){
+                x+=1;
+            }
+        }
+        if (x==0 && y==0){
+            return true;
+        }
+        return false;
+    }
+}
