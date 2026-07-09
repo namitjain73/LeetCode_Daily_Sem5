8c91c69b3c6b2a97e5a7a3bf7f3097ff7388f26b
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:17:03 +0530

    Create 1428-JumpGameIii

---
+++ 1428-JumpGameIii/1428-JumpGameIii.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 5:16:59 PM
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length || arr[start] < 0) {
+            return false;
+        }
+        if (arr[start] == 0) {
+            return true;
+        }
+        int jumpDistance = arr[start];
+        arr[start] = -1; // Mark as visited
+        return canReach(arr, start + jumpDistance) || canReach(arr, start - jumpDistance);
+    }
+}
