89da18b5b9a17f66c080f8b4ece78bc42c07ba62
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:11:27 +0530

    Create 2309-MaximizeNumberOfSubsequencesInAString

---
+++ 2309-MaximizeNumberOfSubsequencesInAString/2309-MaximizeNumberOfSubsequencesInAString.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 5:11:23 PM
+class Solution {
+
+    public long maximumSubsequenceCount(String s, String p) {
+        long ans = 0, max = 0;
+        int cnt = 1;
+        //Assume adding 0th character of pattern at starting
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == p.charAt(1)) ans += cnt;
+            if (s.charAt(i) == p.charAt(0)) cnt++;
+        }
+        max = Math.max(max, ans);
+        ans = 0; cnt = 1;
+        //Assume adding second character of pattern at end
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == p.charAt(0)) ans += cnt;
+            if (s.charAt(i) == p.charAt(1)) cnt++;
+        }
+        max = Math.max(max, ans);
+        return max;
+    }
+}
