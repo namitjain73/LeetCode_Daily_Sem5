055e73b29fc51af789b238b93a1301d67b47ac12
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:12:04 +0530

    Create 2243-CheckIfAllAsAppearsBeforeAllBs

---
+++ 2243-CheckIfAllAsAppearsBeforeAllBs/2243-CheckIfAllAsAppearsBeforeAllBs.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 5:12:00 PM
+class Solution {
+    public boolean checkString(String s) {
+        for(int i = 0 ; i < s.length() - 1 ; i++){
+            if(s.charAt(i) == 'b' && s.charAt(i + 1) == 'a'){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
