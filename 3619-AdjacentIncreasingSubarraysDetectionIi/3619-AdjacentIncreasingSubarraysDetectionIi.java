e41034c291b0b534ec2678484098395d1baecb65
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:03:52 +0530

    Create 3619-AdjacentIncreasingSubarraysDetectionIi

---
+++ 3619-AdjacentIncreasingSubarraysDetectionIi/3619-AdjacentIncreasingSubarraysDetectionIi.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 5:03:48 PM
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int max= 0;
+        int curr = 1;
+        int pre = 0;
+        for(int i = 1 ; i< nums.size() ; i++){
+            if(nums.get(i) > nums.get(i-1)){
+                curr++;
+            }else{
+                max = Math.max(max,Math.max(curr/2,Math.min(pre,curr)));
+                pre = curr;
+                curr = 1;
+            }
+        }
+
+        max = Math.max(max,Math.max(curr/2,Math.min(pre,curr)));
+        return max;
+    }
+}
