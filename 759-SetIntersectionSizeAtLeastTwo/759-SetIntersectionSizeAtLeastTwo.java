d68345f516f32cb1e29cfb2ba7c52dbba16a8dc7
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:20:04 +0530

    Create 759-SetIntersectionSizeAtLeastTwo

---
+++ 759-SetIntersectionSizeAtLeastTwo/759-SetIntersectionSizeAtLeastTwo.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 5:20:00 PM
+class Solution {
+    public int intersectionSizeTwo(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[1] == b[1]) return b[0] - a[0];
+            return a[1] - b[1];
+        });
+        int p1 = -1, p2 = -1; 
+        int count = 0;
+        for (int[] it : intervals) {
+            int s = it[0];
+            int e = it[1];
+            if (p2 >= s) {
+                continue;
+            }
+            if (p1 >= s) {
+                count++;
+                p2 = p1;
+                p1 = e;
+            } else {
+                count += 2;
+                p2 = e - 1;
+                p1 = e;
+            }
+        }
+        return count;
+    }
+}
