cf8e12012852f974808c7fd24d297807e7a73a55
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:11:38 +0530

    Create 2305-AppendKIntegersWithMinimalSum

---
+++ 2305-AppendKIntegersWithMinimalSum/2305-AppendKIntegersWithMinimalSum.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 5:11:34 PM
+class Solution {
+    public long minimalKSum(int[] nums, int k) {
+    Arrays.sort(nums);
+    Set<Integer> set = new HashSet<>();
+    long sum = 0;
+    for (int num: nums) {
+        if (!set.contains(num) && num<=k) {
+            k++;
+            sum += num;        
+        }            
+        set.add(num);
+    } 
+    long res = (long)(1 + k) * k / 2 - sum;
+    return res;             
+    }
+}
