5ecc15d018d03fc54b893bbba18078ca350f4cd9
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:11:58 +0530

    Create 2251-NumberOfWaysToDivideALongCorridor

---
+++ 2251-NumberOfWaysToDivideALongCorridor/2251-NumberOfWaysToDivideALongCorridor.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 5:11:54 PM
+class Solution {
+    public int numberOfWays(String corridor) {
+        int MOD = (int) 1e9 + 7;
+        long ans = 1;
+        int cnt = 0, prev = -1;
+        
+        for (int i = 0; i < corridor.length(); i++) {
+            if (corridor.charAt(i) == 'S') {
+                cnt++;
+                if (cnt % 2 == 1 && cnt > 2) {
+                    ans = ans * (i - prev) % MOD;
+                }
+                if (cnt % 2 == 0) prev = i;
+            }
+        }
+        
+        return cnt % 2 == 0 && cnt > 0 ? (int)ans : 0;
+    }
+}
