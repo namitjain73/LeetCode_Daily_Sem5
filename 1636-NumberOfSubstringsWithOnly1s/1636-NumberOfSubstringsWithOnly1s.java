e10b266494ea00c0876f5fba7d8ead976dd33e0c
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:15:54 +0530

    Create 1636-NumberOfSubstringsWithOnly1s

---
+++ 1636-NumberOfSubstringsWithOnly1s/1636-NumberOfSubstringsWithOnly1s.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 5:15:50 PM
+class Solution {
+    public int numSub(String s) {
+        long sum = 0;
+        int l = 0;
+        long mod = 1000000007;
+        for(int r= 0; r < s.length() ; r++){
+            if(s.charAt(r)  == '0'){
+                continue;
+            }
+            if(r > 0 && s.charAt(r-1) == '0') l = r;
+            sum = (sum + (r - l + 1 )) % mod;
+        }
+        return (int)sum;
+    }
+}
