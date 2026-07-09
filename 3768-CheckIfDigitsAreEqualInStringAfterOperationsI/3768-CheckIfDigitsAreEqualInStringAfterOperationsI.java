8fe885041e797153c49784a2cae2c97f2345c88c
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:02:08 +0530

    Create 3768-CheckIfDigitsAreEqualInStringAfterOperationsI

---
+++ 3768-CheckIfDigitsAreEqualInStringAfterOperationsI/3768-CheckIfDigitsAreEqualInStringAfterOperationsI.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 5:02:04 PM
+class Solution {
+    public boolean hasSameDigits(String s) {
+        while (s.length() > 2) {
+            StringBuilder t = new StringBuilder();
+            for (int i = 0; i + 1 < s.length(); i++) {
+                t.append((s.charAt(i) - '0' + s.charAt(i + 1) - '0') % 10);
+            }
+            s = t.toString();
+        }
+        return s.charAt(0) == s.charAt(1);
+    }
+}
