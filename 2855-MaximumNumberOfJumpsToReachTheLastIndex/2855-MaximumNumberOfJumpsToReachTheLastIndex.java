335aa0060a3601d7ef7f99db68067695817e49d0
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:07:48 +0530

    Create 2855-MaximumNumberOfJumpsToReachTheLastIndex

---
+++ 2855-MaximumNumberOfJumpsToReachTheLastIndex/2855-MaximumNumberOfJumpsToReachTheLastIndex.java
@@ -0,0 +1,33 @@
+// Last updated: 7/9/2026, 5:07:44 PM
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+
+        int[] dp = new int[nums.length];
+
+        // -1 means unreachable
+        Arrays.fill(dp, -1);
+
+        // starting index
+        dp[0] = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // skip unreachable indices
+            if (i > 0 && dp[i] <= 0) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                // valid jump condition
+                if (Math.abs(nums[i] - nums[j]) <= Math.abs(target)) {
+
+                    // maximize number of jumps
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+
+        return dp[nums.length - 1];
+    }
+}
