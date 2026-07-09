614b1eaec30a13779302b444ad1a73bd0adac2a3
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:15:29 +0530

    Create 1704-SpecialPositionsInABinaryMatrix

---
+++ 1704-SpecialPositionsInABinaryMatrix/1704-SpecialPositionsInABinaryMatrix.java
@@ -0,0 +1,30 @@
+// Last updated: 7/9/2026, 5:15:25 PM
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] rowcnt = new int[m];
+        int[] colcnt = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    rowcnt[i]++;
+                    colcnt[j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowcnt[i] == 1 && colcnt[j] == 1) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
