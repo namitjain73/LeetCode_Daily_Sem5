7e31227839ca608cfc94d58bf336f661083d9d52
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:05:03 +0530

    Create 3446-FindTheNumberOfGoodPairsI

---
+++ 3446-FindTheNumberOfGoodPairsI/3446-FindTheNumberOfGoodPairsI.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 5:04:59 PM
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int ans = 0;
+        for(int i = 0 ; i < nums1.length ; i++){
+            for(int j = 0 ; j < nums2.length ; j++){
+                if(nums1[i] % (nums2[j]*k) == 0){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
