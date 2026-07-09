9caa3c71f574382aaec6f29deb1f4f15cc251c80
Namit jain <namitjain9838@gmail.com>
2026-07-09 16:59:46 +0530

    Create 3958-MinimumRemovalsToBalanceArray

---
+++ 3958-MinimumRemovalsToBalanceArray/3958-MinimumRemovalsToBalanceArray.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 4:59:42 PM
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int l = 0, maxsize = 0;
+        for (int r = 0; r < n; r++) {
+            while (l <= r && (long) nums[r] > (long) nums[l] * k)
+                l++;
+            if (r - l + 1 > maxsize) 
+                maxsize = r - l + 1;
+        }
+        return n - maxsize;
+    }
+}
