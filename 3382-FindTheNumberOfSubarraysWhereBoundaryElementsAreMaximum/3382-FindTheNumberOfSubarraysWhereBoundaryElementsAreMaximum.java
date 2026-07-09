2f63f368d7214a323807cbd7f6a024ef32e415bd
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:05:34 +0530

    Create 3382-FindTheNumberOfSubarraysWhereBoundaryElementsAreMaximum

---
+++ 3382-FindTheNumberOfSubarraysWhereBoundaryElementsAreMaximum/3382-FindTheNumberOfSubarraysWhereBoundaryElementsAreMaximum.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 5:05:29 PM
+class Solution {
+    public long numberOfSubarrays(int[] A) {
+        ArrayDeque<int[]> stack = new ArrayDeque<>();
+        long res = 0;
+        for (int a : A) {
+            while (!stack.isEmpty() && stack.peek()[0] < a)
+                stack.pop();
+            if (stack.isEmpty() || stack.peek()[0] != a)
+                stack.push(new int[]{a, 0});
+            res += ++stack.peek()[1];
+        }
+        return res;
+    
+    }
+}
