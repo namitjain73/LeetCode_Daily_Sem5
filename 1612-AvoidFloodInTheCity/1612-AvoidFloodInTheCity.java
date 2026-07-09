2d7308be15f4aa0b14320cd03c0d7b18b89f28e3
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:16:03 +0530

    Create 1612-AvoidFloodInTheCity

---
+++ 1612-AvoidFloodInTheCity/1612-AvoidFloodInTheCity.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 5:15:59 PM
+class Solution {
+
+    public int[] avoidFlood(int[] rains) {
+        int[] ans = new int[rains.length];
+        Arrays.fill(ans, 1);
+        TreeSet<Integer> st = new TreeSet<Integer>();
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for (int i = 0; i < rains.length; ++i) {
+            if (rains[i] == 0) {
+                st.add(i);
+            } else {
+                ans[i] = -1;
+                if (mp.containsKey(rains[i])) {
+                    Integer it = st.ceiling(mp.get(rains[i]));
+                    if (it == null) {
+                        return new int[0];
+                    }
+                    ans[it] = rains[i];
+                    st.remove(it);
+                }
+                mp.put(rains[i], i);
+            }
+        }
+        return ans;
+    }
+}
