c1044d250d31b341e0325fc06f940aa59bab2e5e
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:06:52 +0530

    Create 3211-FindMaximumNonDecreasingArrayLength

---
+++ 3211-FindMaximumNonDecreasingArrayLength/3211-FindMaximumNonDecreasingArrayLength.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 5:06:48 PM
+class Solution {
+        public int findMaximumLength(int[] A) {
+        int n = A.length;
+        int[] pre = new int[n + 2], dp = new int[n + 1];
+        long[] acc = new long[n + 1];
+        for (int i = 1; i <= n; i ++) {
+            acc[i] = acc[i - 1] + A[i - 1];
+        }
+        for (int i = 0, j = 1; j <= n; j++) {
+            i = Math.max(i, pre[j]); 
+            dp[j] = dp[i] + 1;
+            int k = Arrays.binarySearch(acc, 2 * acc[j] - acc[i]);
+            if (k < 0) {
+                k = -k - 1;
+            }
+            pre[k] = j;
+        }
+        return dp[n];
+    }
+}
