e0e31f26647d1a9f718171140f6cd9c45b542172
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:07:03 +0530

    Create 3058-MaximumNumberOfKDivisibleComponents

---
+++ 3058-MaximumNumberOfKDivisibleComponents/3058-MaximumNumberOfKDivisibleComponents.java
@@ -0,0 +1,45 @@
+// Last updated: 7/9/2026, 5:06:59 PM
+class Solution {
+    private List<Integer>[] graph;
+    private int[] values;
+    private int k;
+    private int count;
+
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        this.values = values;
+        this.k = k;
+        this.count = 0;
+
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        dfs(0, -1);
+
+        return count;
+    }
+
+    public long dfs(int node, int parent) {
+        long total = values[node];
+
+        for (int neighbor : graph[node]) {
+            if (neighbor != parent) {
+                total += dfs(neighbor, node);
+            }
+        }
+
+        if (total % k == 0) {
+            count += 1;
+            return 0;
+        }
+
+        return total;
+    }
+}
