70b9ded999011e09ed1468b90fa27ffb9531bb72
Namit jain <namitjain9838@gmail.com>
2026-07-09 17:15:06 +0530

    Create 1784-MinimumInitialEnergyToFinishTasks

---
+++ 1784-MinimumInitialEnergyToFinishTasks/1784-MinimumInitialEnergyToFinishTasks.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 5:15:02 PM
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+
+        int curr = 0;
+        int ans  = 0;
+        for (int[] task : tasks) {
+            int actual  = task[0];
+            int minimum = task[1];
+            if (curr < minimum) {
+                ans  += (minimum - curr);
+                curr  = minimum;
+            }
+            curr -= actual;
+        }
+        return ans;
+    }
+}
