711c66e5a65f3542afee47b373290e99f91d316d
Namit jain <namitjain9838@gmail.com>
2026-08-11 12:16:35 +0530

    File updated at 8/11/2026, 12:16:33 PM

--- 1406-StoneGameIII/1406-StoneGameIII.java
+++ 1406-StoneGameIII/1406-StoneGameIII.java
@@ -1,24 +1,20 @@
-// Last updated: 8/10/2026, 10:22:19 AM
+// Last updated: 8/11/2026, 12:16:33 PM
 1class Solution {
-2    public String stoneGameIII(int[] piles) {
-3        int n = piles.length;
+2    public String stoneGameIII(int[] nums) {
+3        int n = nums.length;
 4        int[] dp = new int[n];
 5        Arrays.fill(dp , -1);
-6        int sum = 0;
-7        for(int i : piles) sum += i;
-8        int ans = solver(piles , 0 , dp);
-9        System.out.println(ans);
-10        return ans > 0 ? "Alice" : ans == 0 ? "Tie" : "Bob";
-11    }
-12    public int solver(int[] arr , int i, int[] dp ){
-13        if(i >= arr.length) return 0;
-14        if(dp[i] != -1) return dp[i];
-15
-16        
-17        int ans = arr[i] - solver(arr , i+1 , dp );
-18        if(i+1 < arr.length) ans = Math.max( arr[i] + arr[i+1] - solver(arr , i+2 , dp ),ans);
-19        if(i+2 < arr.length) ans = Math.max( arr[i] + arr[i+1] + arr[i+2] - solver(arr , i+3 , dp ),ans);
-20        
-21        return dp[i] = ans;
-22    }
-23}
+6        int x = solver(nums,  0 , dp);
+7        System.out.println(x);
+8        return x == 0 ? "Tie" : x > 0 ? "Alice" : "Bob";
+9    }
+10    public int solver(int[] arr, int i ,  int[] dp){
+11        if(i >= arr.length) return 0;
+12        if(dp[i] != -1) return dp[i];
+13
+14        int res = arr[i] - solver(arr , i+1 , dp);
+15        if(i+1 < arr.length) res = Math.max(res , arr[i] + arr[i + 1] - solver(arr , i+2 , dp));
+16        if(i+2 < arr.length) res = Math.max(res , arr[i] + arr[i+1] + arr[i+2] - solver(arr , i+3 , dp));
+17        return dp[i] = res;
+18    }
+19}
