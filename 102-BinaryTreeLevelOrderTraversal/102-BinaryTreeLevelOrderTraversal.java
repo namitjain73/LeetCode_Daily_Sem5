0bb9a77c1ea2477057b8c0a1aaccb54306511fa6
Namit jain <namitjain9838@gmail.com>
2026-08-16 16:30:04 +0530

    File updated at 8/16/2026, 4:30:04 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,44 +1,32 @@
-// Last updated: 7/9/2026, 5:24:35 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int level(TreeNode root){
-        if(root == null) return 0;
-        // if(root.left == null&& root.right == null) return 0;
-        return 1+Math.max(level(root.left),level(root.right));
-
-    }
-    public void allLevels(TreeNode root , List<Integer> list, int n){
-        if(root == null) return;
-        if(n == 1) {
-            list.add(root.val);
-            return;
-        }
-        allLevels(root.left,list,n-1);
-        allLevels(root.right,list,n-1);
-
-    }
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ll = new ArrayList<>();
-        int level = level(root);
-        for(int i = 1 ; i <= level ; i++){
-            List<Integer> list = new ArrayList<>();
-            allLevels(root,list,i);
-            ll.add(list);
-        }
-        return ll;
-    }
-}
+// Last updated: 8/16/2026, 4:30:04 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<List<Integer>> levelOrder(TreeNode root) {
+18        List<List<Integer>> ll = new ArrayList<>();
+19        solver(root , 1 , ll);
+20        return ll;
+21    }
+22    public void solver(TreeNode root , int n , List<List<Integer>> ll){
+23        if(root == null) return;
+24
+25        if(ll.size() < n) ll.add(new ArrayList<>());
+26        ll.get(n-1).add(root.val);
+27
+28        solver(root.left , n+1 , ll);
+29        solver(root.right , n+1 , ll);
+30    }
+31}
