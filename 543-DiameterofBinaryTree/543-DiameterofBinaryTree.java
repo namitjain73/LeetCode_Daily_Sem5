74020d5f351e8a1a79091bf7514969548e0b60cd
Namit jain <namitjain9838@gmail.com>
2026-08-19 01:58:02 +0530

    File updated at 8/19/2026, 1:58:00 AM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 8/19/2026, 1:56:35 AM
+// Last updated: 8/19/2026, 1:58:00 AM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,22 +15,22 @@
 14 * }
 15 */
 16class Solution {
-17    int ans;
+17    int n;
 18    public int minCameraCover(TreeNode root) {
-19        ans = 0;
-20        if(solver(root) == -1) ans++;
-21        return ans;
+19        n = 0;
+20        int ans = solver(root);
+21        return ans == -1 ? n+1 : n;
 22    }
 23    public int solver(TreeNode root){
 24        if(root == null) return 0;
 25
-26
-27        int left = solver(root.left);
-28        int right = solver(root.right);
-29        if(left == -1 || right == -1) {
-30            ans++;
+26        int left = solver(root.left);
+27        int right= solver(root.right);
+28
+29        if(left == -1 || right == -1){
+30            n++;
 31            return 1;
 32        }else if(left == 1 || right == 1) return 0;
-33        else return -1;
+33        else  return -1;
 34    }
 35}
