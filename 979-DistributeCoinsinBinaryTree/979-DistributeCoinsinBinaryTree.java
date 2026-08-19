6f858288b12e3e60a3a42f962ae2a66be4d2499a
Namit jain <namitjain9838@gmail.com>
2026-08-19 14:29:34 +0530

    File updated at 8/19/2026, 2:29:31 PM

--- 979-DistributeCoinsinBinaryTree/979-DistributeCoinsinBinaryTree.java
+++ 979-DistributeCoinsinBinaryTree/979-DistributeCoinsinBinaryTree.java
@@ -1,34 +1,32 @@
-// Last updated: 8/22/2025, 12:07:43 AM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int ans = 0;
-
-    public int helper(TreeNode root){
-        if(root == null) return 0;
-
-        int left = helper(root.left);
-        int right = helper(root.right);
-
-        ans += Math.abs(left) + Math.abs(right);
-        return root.val + left + right - 1;
-    }
-    public int distributeCoins(TreeNode root) {
-        ans = 0;
-        helper(root);
-        return ans;
-    }
-}
+// Last updated: 8/19/2026, 2:29:31 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int ans;
+18    public int distributeCoins(TreeNode root) {
+19        ans = 0;
+20        solver(root );
+21        return ans;
+22    }
+23    public int solver(TreeNode root ){
+24        if(root == null) return 0;
+25
+26        int left = solver(root.left );
+27        int right = solver(root.right );
+28        ans += Math.abs(left) + Math.abs(right);
+29        return root.val + left + right - 1;
+30    }
+31}
