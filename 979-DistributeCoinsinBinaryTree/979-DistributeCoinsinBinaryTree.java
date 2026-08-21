009f5ab4ff9ba5446850e6a423edd46284a701e6
Namit jain <namitjain9838@gmail.com>
2026-08-21 22:25:15 +0530

    File updated at 8/21/2026, 10:25:15 PM

--- 979-DistributeCoinsinBinaryTree/979-DistributeCoinsinBinaryTree.java
+++ 979-DistributeCoinsinBinaryTree/979-DistributeCoinsinBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 8/19/2026, 2:29:31 PM
+// Last updated: 8/21/2026, 10:25:15 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -17,15 +17,15 @@
 16class Solution {
 17    int ans;
 18    public int distributeCoins(TreeNode root) {
-19        ans = 0;
-20        solver(root );
+19        ans = 0;;
+20        solver(root);
 21        return ans;
 22    }
-23    public int solver(TreeNode root ){
+23    public int solver(TreeNode root){
 24        if(root == null) return 0;
 25
-26        int left = solver(root.left );
-27        int right = solver(root.right );
+26        int left = solver(root.left);
+27        int right = solver(root.right);
 28        ans += Math.abs(left) + Math.abs(right);
 29        return root.val + left + right - 1;
 30    }
